f0d206c653c748db7c79d11b027bac5fb67c528e
akorede <emiolasaheed1@gmail.com>
2023-07-03 11:42:24 +0100

    Add functionality to remove books from UI

--- index.js
+++ index.js
@@ -19,30 +19,40 @@ let bookList = [
     id: 3,
   },
 ];
-
+// Add book to booklist
 function addBook(title , author, id){
    const newBook = {title , author, id};
    bookList.push(newBook);
 }
-
+// remove books from booklist
 function removeBook(id) {
   bookList = bookList.filter(book => book.id !== id);
 }
-
+// Display books
 function displayBook() {
   bookListContainer.innerHTML = '';
   const bookElement = bookList.map((book) => {
     const bookCard = `<li>
     <p>${book.title}</p>
     <p>${book.author}</p>
-    <button id=${book.id}>Remove</button>
+    <button class="remove-btn" id=${book.id}>Remove</button>
   </li>`;
   return bookCard;
   }).join('')
-  bookListContainer.insertAdjacentHTML('beforeend',bookElement);
+   bookListContainer.insertAdjacentHTML('beforeend',bookElement);
+   const removeBtns = bookListContainer.querySelectorAll('.remove-btn');
+    removeBtns.forEach((btn) => {
+    btn.addEventListener('click', removeBookFromUI)
+  })
+}
+//Removes book from UI
+function removeBookFromUI(e) {
+  const id = e.target.id;
+  removeBook(+id);
+  displayBook();
 }
 displayBook();
-
+// Add book to UI
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const id = Math.floor(Math.random() * 1000);
@@ -50,4 +60,4 @@ form.addEventListener('submit', (e) => {
   const author = authorInput.value;
   addBook(title, author, id);
   displayBook();
-})
+})``
