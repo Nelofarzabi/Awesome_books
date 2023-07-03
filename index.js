32f9cb5a2e28d1d1abb84789ae76c160cc861b91
akorede <emiolasaheed1@gmail.com>
2023-07-03 09:21:03 +0100

    Create book list

---
+++ index.js
@@ -0,0 +1,14 @@
+const bookList = [
+  {
+    title: 'The five love languages',
+    author: 'Gary Chapman',
+  },
+  {
+    title: 'How to influence people',
+    author: 'Dale Carnegie',
+  },
+  {
+    title: 'The river',
+    author: 'Nelofar Zabi',
+  },
+];
