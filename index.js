0d60cee5b37be06b3207217b6c14a2917c2b088d
akorede <emiolasaheed1@gmail.com>
2023-07-04 10:18:34 +0100

    refactor to use JavaScript classes

--- index.js
+++ index.js
@@ -2,3 +2,62 @@ const bookListContainer = document.querySelector('.book-list');
 const titleInput = document.querySelector('.title');
 const authorInput = document.querySelector('.author');
 const form = document.querySelector('form');
+
+class BookList {
+  constructor() {
+    this.bookArray = [];
+  }
+
+  addBook(title, author, id) {
+    const newBook = { title, author, id};
+    this.bookArray = [...this.bookArray, newBook];
+  }
+  removeBook(id) {
+    this.bookArray = this.bookArray.filter((book) => book.id !== id);
+  }
+  displayBook() {
+    bookListContainer.innerHTML = '';
+    const bookElement = this.bookArray.map((book) => {
+      const bookCard = `<li>
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button class="remove-btn" id=${book.id}>Remove</button>
+    </li>`;
+      return bookCard;
+    }).join('');
+    bookListContainer.insertAdjacentHTML('beforeend', bookElement);
+    const removeBtns = bookListContainer.querySelectorAll('.remove-btn'); 
+    removeBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const {id} = e.target;
+        this.removeBook(+id);
+        this.displayBook();
+        this.addAndUpdateBooksToLocalStorage();
+      });
+    });
+  }
+  addBookToUI() {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const id = Math.floor(Math.random() * 2000);
+      const title = titleInput.value;
+      const author = authorInput.value;
+      this.addBook(title, author, id);
+      this.addAndUpdateBooksToLocalStorage();
+      this.displayBook();
+    });
+  }
+  addAndUpdateBooksToLocalStorage() {
+    localStorage.setItem('bookList', JSON.stringify(this.bookArray));
+  }
+  getBookFromLocalStorage() {
+    return localStorage.getItem('bookList') ? JSON.parse(localStorage.getItem('bookList')) : [];
+  }
+}
+
+const book = new BookList();
+window.addEventListener('DOMContentLoaded', () => {
+  book.bookArray = book.getBookFromLocalStorage();
+  book.displayBook();
+  book.addBookToUI();
+})
